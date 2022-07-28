1028ab1c5845dfa0fd40054cab82be33abcb38c3
Jason Gross <jgross@mit.edu>
2022-07-27 23:56:02 -0400

    Improve presentation, add animations and videos

--- presentation/initial-bug/build-coq.sh
+++ presentation/initial-bug/build-coq.sh
@@ -8,6 +8,10 @@ git submodule update --init --recursive
 cd HoTT-coq
 git reset --hard
 patch -p1 < ../HoTT-coq-configure.patch
+
+# camlp4              4.01
+# camlp5              6.17
+# The Objective Caml compiler, version 3.12.1
 ./configure -local -camlp5dir "$(ocamlfind query camlp5)" -with-doc no -coqide no
-make -j$J
+make -j$J TIMED=1
 cd ..
