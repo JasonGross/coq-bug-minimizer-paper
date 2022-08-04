89046c4c3f38775ae2e7800756716a567abad708
Jason Gross <jgross@mit.edu>
2022-08-04 10:03:19 +0300

    Add manual minimization datapoints

--- presentation/make-bug-minimizer-reduction-csv.sh
+++ presentation/make-bug-minimizer-reduction-csv.sh
@@ -3,4 +3,5 @@
 { true                                                | ./compute-lines.sh;
   cat test-suite-coq-bug-finder.txt                   | ./compute-lines.sh --no-header test-suite;
   cat ../notebook/presentation/minimization_pairs.csv | ./compute-lines.sh --no-header ci-minimization;
+  cat ../notebook/presentation/issue_comments.csv     | ./compute-lines.sh --no-header manual-issues;
 } > coq-bug-finder-reduction.csv
